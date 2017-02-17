1b76827bd0071f490333d77231601d8c6d4299e2
betahikaru <beta.hikaru2000+github@gmail.com>
2017-02-18 02:18:08 +0900

    parse対象データの可変化

--- scripts/app.js
+++ scripts/app.js
@@ -3,41 +3,50 @@
     var urlsMap = {
         'awsIam': '/json/aws/iam/timeline.json'
     };
+    var parseRulesMap = {
+        'awsIam': {
+            'statusName': 'IamStatus',
+            'dataNames': ['countUsers', 'countGroups', 'countRoles']
+        }
+    };
 
     function run() {
         google.charts.load('current', {packages: ['corechart', 'line']});
-        fetchTimeline(urlsMap.awsIam);
+        fetchTimeline('awsIam');
         window.addEventListener("resize", function(){
             drawIamStatusGraph();
         }, false);
     }
 
-    function fetchTimeline(url) {
+    function fetchTimeline(service) {
+        var url = urlsMap[service];
         fetch(url)
         .then(function(response) {
             return response.json()
         }).then(function(json) {
             console.log('parsed json', json)
-            rowsMap.iamStatusRows = getIamStatusRows(json);
+            rowsMap.iamStatusRows = parseJsonToRows(json, service);
             google.charts.setOnLoadCallback(drawIamStatusGraph);
         }).catch(function(ex) {
             console.log('parsing failed', ex)
         });
     }
 
-    function getIamStatusRows(json) {
+    function parseJsonToRows(json, service) {
         var rows = [];
         if (json && json.list) {
             var list = json.list;
             var size = list.length;
             for (var i=0;i<size;i++) {
-                var status = list[i].IamStatus;
-                var row = [
-                    new Date(list[i].datetime),
-                    status.countUsers,
-                    status.countGroups,
-                    status.countRoles,
-                ];
+                var data = list[i];
+                var statusName = parseRulesMap[service]['statusName'];
+                var dataNames = parseRulesMap[service]['dataNames'];
+                var status = data[statusName];
+                var row = [new Date(data.datetime)];
+                for (var j=0; j<dataNames.length; j++) {
+                    var dataName = dataNames[j];
+                    row.push(status[dataName]);
+                }
                 rows.push(row);
             }
         }
