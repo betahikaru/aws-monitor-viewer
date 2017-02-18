bb647809678409184875226bd6286c9fcd930996
betahikaru <beta.hikaru2000+github@gmail.com>
2017-02-18 10:15:15 +0900

    Refactor variable name and error message

--- scripts/app.js
+++ scripts/app.js
@@ -1,61 +1,71 @@
-(function() {
-    // configure
-    var services = ['awsIam'];
-    var urlsMap = {
-        'awsIam': '/json/aws/iam/timeline.json'
-    };
-    var graphSelectorsMap = {
-        'awsIam': 'iamStatusChart'
-    };
-    var parseRulesMap = {
-        'awsIam': {
+// configure data
+var config = {
+    'awsIam': {
+        'url': '/json/aws/iam/timeline.json',
+        'graphId': 'iamStatusChart',
+        'parseRulesMap': {
             'statusName': 'IamStatus',
             'dataNames': ['countUsers', 'countGroups', 'countRoles']
         }
+    }
+};
+
+(function() {
+    // application data
+    var app = {
+        services: [],
+        dataStore: {}
     };
-    var rowsMap = {};
 
     // initialize
     function run() {
         google.charts.load('current', {packages: ['corechart', 'line']});
-        fetchTimelineAll(services);
+        initializeAppData();
+        fetchTimelineAll();
         window.addEventListener("resize", function(){
-            drawTimelineGraphAll(services);
+            drawTimelineGraphAll();
         }, false);
+    }
+    function initializeAppData() {
+        app.services = [];
+        for(var sName in config) {
+            app.services.push(sName);
+            app.dataStore[sName] = {};
+        }
     }
 
     // fetch
-    function fetchTimelineAll(services) {
-        for (var i=0; i<services.length; i++) {
-            var service = services[i];
-            fetchTimelineByServiceName(service);
+    function fetchTimelineAll() {
+        for (var i = 0; i < app.services.length; i++) {
+            var sName = app.services[i];
+            fetchTimelineByServiceName(sName);
         }
     }
-    function fetchTimelineByServiceName(service) {
-        var url = urlsMap[service];
+    function fetchTimelineByServiceName(sName) {
+        var url = config[sName]['url'];
         fetch(url)
         .then(function(response) {
             return response.json()
         }).then(function(json) {
-            console.log('parsed json', json)
-            var rows = parseJsonToRows(json, service);
-            rowsMap[service] = rows;
-            google.charts.setOnLoadCallback(drawTimelineGraphByServiceName.bind(null, service));
+            console.log('[INFO] Parsed json for service timeline', sName, json);
+            var rows = parseJsonToRows(json, sName);
+            app.dataStore[sName]['rowsMap'] = rows;
+            google.charts.setOnLoadCallback(drawTimelineGraphByServiceName.bind(null, sName));
         }).catch(function(ex) {
-            console.log('parsing failed', ex)
+            console.log('[INFO] Parsing failed for service timeline', sName, ex);
         });
     }
 
     // parse
-    function parseJsonToRows(json, service) {
+    function parseJsonToRows(json, sName) {
         var rows = [];
         if (json && json.list) {
             var list = json.list;
             var size = list.length;
             for (var i=0;i<size;i++) {
                 var data = list[i];
-                var statusName = parseRulesMap[service]['statusName'];
-                var dataNames = parseRulesMap[service]['dataNames'];
+                var statusName = config[sName]['parseRulesMap']['statusName'];
+                var dataNames = config[sName]['parseRulesMap']['dataNames'];
                 var status = data[statusName];
                 var row = [new Date(data.datetime)];
                 for (var j=0; j<dataNames.length; j++) {
@@ -69,22 +79,22 @@
     }
 
     // draw graph
-    function drawTimelineGraphAll(services) {
-        for (var i=0; i<services.length; i++) {
-            var service = services[i];
-            drawTimelineGraphByServiceName(service);
+    function drawTimelineGraphAll() {
+        for (var i = 0; i < app.services.length; i++) {
+            var sName = app.services[i];
+            drawTimelineGraphByServiceName(sName);
         }
     }
-    function drawTimelineGraphByServiceName(service) {
-        var data = new google.visualization.DataTable();
-        data.addColumn('date', 'Time');
-        var dataNames = parseRulesMap[service]['dataNames'];
-        for (var i=0;i<dataNames.length;i++) {
+    function drawTimelineGraphByServiceName(sName) {
+        var dataTable = new google.visualization.DataTable();
+        dataTable.addColumn('date', 'Time');
+        var dataNames = config[sName]['parseRulesMap']['dataNames'];
+        for (var i = 0; i < dataNames.length; i++) {
             var dataName = dataNames[i];
-            data.addColumn('number', dataName);
+            dataTable.addColumn('number', dataName);
         }
-        data.addRows(rowsMap[service]);
-        var chartElm = document.getElementById(graphSelectorsMap[service]);
+        dataTable.addRows(app.dataStore[sName]['rowsMap']);
+        var chartElm = document.getElementById(config[sName]['graphId']);
         var chart = new google.visualization.LineChart(chartElm);
         var options = {
             hAxis: {
@@ -100,7 +110,7 @@
                 height:'80%'
             }
         };
-        chart.draw(data, options);
+        chart.draw(dataTable, options);
     }
 
     // run
